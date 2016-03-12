3c4d780231790fdf6fde03e06ead789db4c45c70
Alexander Hwang <kuteragenesis@yahoo.com>
2016-03-11 18:05:29 -0800

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,9 +1,11 @@
 __author__ = 'KG'
-from datetime import datetime
 
 hello = "Hello world."
 print(hello)
-print(hello[0])
+count = 0
+while (count < len(hello)):
+    print(hello[count])
+    count += 1
 
 
 val = len(hello)
@@ -17,13 +19,3 @@ val *= val
 print(val)
 print(str(val) + " + " + str(val * 2) + " = " + str(val * 3))
 
-junk = datetime.now().second
-pick = "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"[junk]
-junk = datetime.now().minute / datetime.now().second
-if (len(str(junk)) > 8):
-    junk *= len(str(junk)) + 1
-elif (len(str(junk)) > 4):
-    junk *= len(str(junk))
-else:
-    junk += len(str(junk))
-print(str(junk) + pick)
