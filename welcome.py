50debefa2aa781b74bd623071574abfc3f42414b
Alexander Hwang <kuteragenesis@yahoo.com>
2016-03-14 21:20:05 -0700

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -8,14 +8,26 @@ while (count < len(hello)):
     count += 1
 
 
+print()
 val = len(hello)
 
 print(val)
 
+print()
 val *= len(hello)
 val += val
 val *= val
 
 print(val)
 print(str(val) + " + " + str(val * 2) + " = " + str(val * 3))
+print()
 
+data = input("Enter something: ")
+
+if len(data) > 0 and data.isalpha() and data != "no":
+    print(data)
+elif data == "no":
+    print("")
+else:
+    print("no")
+print()
