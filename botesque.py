2ced35d866b3eb22269e2ebaa72077c352b7fe92
Alexander Hwang <kuteragenesis@yahoo.com>
2016-03-21 20:02:31 -0700

    Update botesque.py

--- botesque.py
+++ botesque.py
@@ -1,6 +1,12 @@
 __author__ = 'KG'
+
+# Initial greeting needs work.
+
 print("Hello.")
 immData = "" # Immediate data
+prevData = "" # Previous data
+flags = ["1"] # 1 = greeting
+flagData = ["Hi."]
 while (immData != "Done." and immData != "Finished." and immData != "End."):
     immData = input("Response: ")
     immLen = len(immData)
@@ -18,3 +24,13 @@ while (immData != "Done." and immData != "Finished." and immData != "End."):
         immData += "."
     print(immData)
     print()
+    if immData == "That is a greeting." or immData == "That's a greeting.":
+        flags.extend("1")
+        flagData.append(prevData)
+    index = 0
+    for x in flagData:
+        if (x == immData):
+            if (flags[index] == "1"):
+                print((flagData[index])[:(len(prevData) - 1)] + " to you too.")
+        index += 1
+    prevData = immData
