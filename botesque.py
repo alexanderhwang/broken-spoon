aa3a59cf51286b8d2404f425661a0b5db39a7bb2
Alexander Hwang <kuteragenesis@yahoo.com>
2016-03-28 18:27:38 -0700

    Update botesque.py

--- botesque.py
+++ botesque.py
@@ -1,6 +1,6 @@
 __author__ = 'KG'
-
-print("Hello.")
+mol = "\033[1mMol: \033[0m"
+print(mol + "Hello.")
 immData = "" # Immediate data
 prevData = "" # Previous data
 flags = ["1", "2"] # 1 = greeting, # 2 = farewell
@@ -9,7 +9,7 @@ while (immData != "Done." and immData != "Finished." and immData != "End."):
     immData = input("Response: ")
     immLen = len(immData)
     if immLen == 0:
-        print("Please say something. If you're done, say \"Done\".")
+        print(mol + "Please say something. If you're done, say \"Done\".")
     while immData[0] == " " and immLen > 0:
         immData = immData[1:]
         immLen -= 1
@@ -25,18 +25,18 @@ while (immData != "Done." and immData != "Finished." and immData != "End."):
     if immData == "That is a greeting." or immData == "That's a greeting.":
         flags.extend("1")
         flagData.append(prevData)
-        print("Oh okay. " + prevData)
+        print(mol + "Oh okay. " + prevData)
     elif immData == "That is a farewell." or immData == "That's a farewell.":
         flags.extend("2")
         flagData.append(prevData)
-        print("Oh okay.")
+        print(mol + "Oh okay.")
     index = 0
     for x in flagData:
         if (x == immData):
             if (flags[index] == "1"):
-                print((flagData[index])[:(len(flagData[index]) - 1)] + " to you too.")
+                print(mol + (flagData[index])[:(len(flagData[index]) - 1)] + " to you too.")
             if (flags[index] == "2"):
-                print(flagData[index])
+                print(mol + flagData[index])
                 immData = "Done."
         index += 1
     prevData = immData
